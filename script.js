45d5d344e265d947d65c5bfae4fc8f04b04a1871
pierre-slimm <mainapeter008@gmail.com>
2025-01-08 13:19:05 +0300

    ddd

--- script.js
+++ script.js
@@ -73,3 +73,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
 });
+let slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function nextSlide() {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function startSlideshow(){
+  setInterval(nextSlide, 3000); // Change slide every 3000 milliseconds
+}
+
+window.onload = function() {
+  startSlideshow();
+}
